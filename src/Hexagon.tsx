9f1119ee7e94ede05a67c29ca562eaed9d5dc802
Dallin Abendroth <dallin.aben@gmail.com>
2023-07-13 16:19:14 -0600

    some reasonable performance improvement

--- src/Hexagon.tsx
+++ src/Hexagon.tsx
@@ -2,19 +2,28 @@ import React from 'react';
 import { HexagonStyled } from './Hexagon.styled';
 
 type HexagonProps = {
-  toggled: boolean,
-  onClick: (arg0: number, arg1: number) => void,
-  hexSize: number,
   row: number,
   col: number,
+  hexSize: number,
+  toggled: boolean,
+  onClick: (row: number, col: number) => void,
 };
 
-const Hexagon: React.FC<HexagonProps> = React.memo(({ row, col, hexSize, toggled, onClick }) => (
-  <HexagonStyled 
-    onClick={() => onClick(row, col)} 
-    toggled={toggled} 
-    hexSize={hexSize} 
+const Hexagon: React.FC<HexagonProps> = React.memo(
+  ({ row, col, hexSize, toggled, onClick }) => (
+    <HexagonStyled
+      onClick={() => onClick(row, col)}
+      toggled={toggled}
+      hexSize={hexSize}
     />
-));
+  ), 
+  // Add a comparison function to prevent unnecessary re-renders
+  (prevProps, nextProps) => (
+    prevProps.row === nextProps.row && 
+    prevProps.col === nextProps.col && 
+    prevProps.hexSize === nextProps.hexSize && 
+    prevProps.toggled === nextProps.toggled
+  )
+);
 
 export default Hexagon;
