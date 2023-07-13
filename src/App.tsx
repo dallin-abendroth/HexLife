addfbd3b46240b993901ea84222c8082483c50ad
Dallin Abendroth <dallin.aben@gmail.com>
2023-07-13 11:43:18 -0600

    black background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import HexGrid from './HexGrid';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+const AppContainer = styled.div`
+  background-color: black;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <HexGrid rows={10} cols={10} />
-    </div>
+    <AppContainer>
+      <HexGrid rows={20} cols={20} />
+    </AppContainer>
   );
 }
 
