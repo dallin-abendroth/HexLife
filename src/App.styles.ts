0a88ef85c500e049d00603b59254f0ace9bfdb9b
Dallin Abendroth <dallin.aben@gmail.com>
2023-07-13 20:33:23 -0600

    cleanup default react index.html

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -26,3 +26,23 @@ export const HexGridContainer = styled.div`
   justify-content: center;
   margin-bottom: 20px;
 `;
+
+export const InfoButton = styled.a`
+  font-family: 'Helvetica';
+  font-size: 10px;
+  color: white;
+  text-decoration: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15px;
+  width: 15px;
+  border-radius: 50%;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: white;
+    color: black;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import HexGrid from './HexGrid';
-import { AppContainer, HexGridContainer, Title } from './App.styles';
-
+import { AppContainer, HexGridContainer, Title, InfoButton } from './App.styles';
 
 const App: React.FC = () => {
   return (
     <AppContainer>
       <Title>Conway's Game of Life with Hexagons</Title>
+      <InfoButton href="https://github.com/dallin-abendroth/HexagonLife" target="_blank">ⓘ</InfoButton>
       <HexGridContainer>
         <HexGrid rows={60} cols={80} hexSize={10} />
       </HexGridContainer>
